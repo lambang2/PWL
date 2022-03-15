a815e270b24b199728b56838fabf37b877b28342
lambang2 <93249848+lambang2@users.noreply.github.com>
2022-03-15 17:18:09 +0700

    Add files via upload

---
+++ minggu1/Latihan/2.php
@@ -0,0 +1,6 @@
+<?php 
+$nim = "A11.2020.80009";
+$nama = 'Lambang';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama";
+?>
---
+++ minggu1/Latihan/4.php
@@ -0,0 +1,6 @@
+<?php
+define ("NAMA", "Lambang");
+define ("NILAI", 90);
+echo "Nama : " . NAMA;
+echo "<br>Nilai : " . NILAI;
+?>
